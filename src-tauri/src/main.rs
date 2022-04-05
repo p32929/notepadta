9505d054b34137841526b309e2c45a3bd74aa944
p32929 <p32929@yahoo.com>
2022-04-06 01:48:35 +0600

    rem menus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,23 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{Menu, MenuItem, Submenu};
-
 fn main() {
-  let submenu2 = Submenu::new(
-    "Edit",
-    Menu::new()
-      .add_native_item(MenuItem::Copy)
-      .add_native_item(MenuItem::Cut)
-      .add_native_item(MenuItem::Paste)
-      .add_native_item(MenuItem::Undo)
-      .add_native_item(MenuItem::Redo),
-  );
-
-  let menu = Menu::new().add_submenu(submenu2);
-
   tauri::Builder::default()
-    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
